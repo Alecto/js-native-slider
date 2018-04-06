e1bd9b9ddf63c4883f8c51ff9492057f495164d0
Andrey <fomenkoandrey@inbox.ru>
2018-04-06 20:30:15 +0300

    minor improvements with classList.toggle

--- js/main.js
+++ js/main.js
@@ -1,26 +1,41 @@
+
 var controls = document.querySelectorAll('.controls');
 var slides = document.querySelectorAll('.slides__item');
 var indicators = document.querySelectorAll('.indicators__item');
 
 // carousel basic engine
 var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,2000);
 
-function nextSlide(){
-  goToSlide(currentSlide+1);
-}
 
-function previousSlide(){
-  goToSlide(currentSlide-1);
-}
-
-function goToSlide(n){
+var gotoSlide = function (n) {
   slides[currentSlide].classList.toggle('active');
   indicators[currentSlide].classList.toggle('active');
   currentSlide = (n+slides.length)%slides.length;
   slides[currentSlide].classList.toggle('active');
   indicators[currentSlide].classList.toggle('active');
-}
+};
+
+var nextSlide = function () {
+  gotoSlide(currentSlide+1);
+};
+
+var previousSlide = function () {
+  gotoSlide(currentSlide-1);
+};
+
+var pauseSlideShow = function () {
+  pauseButton.innerHTML = '&#9658;'; // play character
+  playing = false;
+  clearInterval(slideInterval);
+};
+
+var playSlideShow = function () {
+  pauseButton.innerHTML = '&#10074;&#10074;'; // pause character
+  playing = true;
+  slideInterval = setInterval(nextSlide,2000);
+};
+
+var slideInterval = setInterval(nextSlide, 2000);
 
 // controls and indicators
 var playing = true;
@@ -28,47 +43,35 @@ var pauseButton = document.getElementById('pause');
 var nextButton = document.getElementById('next');
 var previousButton = document.getElementById('previous');
 
-pauseButton.addEventListener('click', pauseButtonAction);
-nextButton.addEventListener('click', nextSlideAction);
-previousButton.addEventListener('click', previousSlideAction);
-
-function pauseSlideshow(){
-  pauseButton.innerHTML = '&#9658;'; // play character
-  playing = false;
-  clearInterval(slideInterval);
-}
-
-function playSlideshow(){
-  pauseButton.innerHTML = '&#10074;&#10074;'; // pause character
-  playing = true;
-  slideInterval = setInterval(nextSlide,2000);
-}
-
-function pauseButtonAction (){
+var pauseClickHandler = function () {
   if (playing) {
-    pauseSlideshow();
+    pauseSlideShow();
   } else {
-    playSlideshow();
+    playSlideShow();
   }
-}
+};
 
-function nextSlideAction () {
-  pauseSlideshow();
+var nextClickHandler = function () {
+  pauseSlideShow();
   nextSlide();
-}
+};
 
-function previousSlideAction () {
-  pauseSlideshow();
+var previousClickHandler = function () {
+  pauseSlideShow();
   previousSlide();
-}
+};
 
-function goToSlideOnClick () {
+pauseButton.addEventListener('click', pauseClickHandler);
+nextButton.addEventListener('click', nextClickHandler);
+previousButton.addEventListener('click', previousClickHandler);
+
+var indicatorClickHandler = function () {
   var n = this.getAttribute('data-slide-to') - 1;
-  pauseSlideshow();
-  goToSlide(n);
-}
+  pauseSlideShow();
+  gotoSlide(n);
+};
 
-var i;
-for(i=0; i<indicators.length; i++) {
-  indicators[i].addEventListener('click', goToSlideOnClick);
+
+for(var i = indicators.length; i--;) {
+  indicators[i].addEventListener('click', indicatorClickHandler);
 }
