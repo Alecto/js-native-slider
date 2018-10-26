b98dc474455770ec3dcbeb7c5550831775116b7a
Alecto <fomenkoandrey@inbox.ru>
2018-10-26 17:55:53 +0300

    var --> let. рефакторинг путей. обновление gulpfile.js для gulp 4.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,67 +1,67 @@
 'use strict';
 
-var slides        = document.querySelectorAll('.slides__item');
-var indContainer  = document.querySelector('.indicators');
-var indItems      = document.querySelectorAll('.indicators__item');
-var currentSlide  = 0;
+let slideItems    = document.querySelectorAll('.slides__item'),
+    indContainer  = document.querySelector('.indicators'),
+    indItems      = document.querySelectorAll('.indicators__item'),
+    currentSlide  = 0;
 
-const LEFT_ARROW  = 37;
-const RIGHT_ARROW = 39;
-const SPACE       = 32;
-const FA_PAUSE    = '<i class="fas fa-pause"></i>';
-const FA_PLAY     = '<i class="fas fa-play"></i>';
+const LEFT_ARROW  = 37,
+      RIGHT_ARROW = 39,
+      SPACE       = 32,
+      FA_PAUSE    = '<i class="fas fa-pause"></i>',
+      FA_PLAY     = '<i class="fas fa-play"></i>';
 
 // activate controls, if javascript is enabled
 indContainer.style.display = 'flex'; // flex
 document.querySelector('.controls').style.display = 'block'; // block
 
 // carousel basic engine
-var gotoSlide = function (n) {
-  slides[currentSlide].classList.toggle('active');
+let gotoSlide = function (n) {
+  slideItems[currentSlide].classList.toggle('active');
   indItems[currentSlide].classList.toggle('active');
-  currentSlide = (n + slides.length) % slides.length;
-  slides[currentSlide].classList.toggle('active');
+  currentSlide = (n + slideItems.length) % slideItems.length;
+  slideItems[currentSlide].classList.toggle('active');
   indItems[currentSlide].classList.toggle('active');
 };
 
-var nextSlide = function () {
+let nextSlide = function () {
   gotoSlide(currentSlide + 1);
 };
 
-var previousSlide = function () {
+let previousSlide = function () {
   gotoSlide(currentSlide - 1);
 };
 
-var pauseSlideShow = function () {
+let pauseSlideShow = function () {
   pauseBtn.innerHTML = FA_PAUSE;
   playbackStatus = false;
   clearInterval(slideInterval);
 };
 
-var playSlideShow = function () {
+let playSlideShow = function () {
   pauseBtn.innerHTML = FA_PLAY;
   playbackStatus = true;
   slideInterval = setInterval(nextSlide, 2000);
 };
 
-var slideInterval = setInterval(nextSlide, 2000);
+let slideInterval = setInterval(nextSlide, 2000);
 
 // controls
-var playbackStatus = true;
-var pauseBtn = document.querySelector('.indicators__pause');
-var nextBtn  = document.querySelector('.controls__next');
-var prevBtn  = document.querySelector('.controls__prev');
+let playbackStatus = true,
+    pauseBtn = document.querySelector('.indicators__pause'),
+    nextBtn  = document.querySelector('.controls__next'),
+    prevBtn  = document.querySelector('.controls__prev');
 
-var pauseClickHandler = function () {
+let pauseClickHandler = function () {
   playbackStatus ? pauseSlideShow() : playSlideShow();
 };
 
-var nextClickHandler = function () {
+let nextClickHandler = function () {
   pauseSlideShow();
   nextSlide();
 };
 
-var prevClickHandler = function () {
+let prevClickHandler = function () {
   pauseSlideShow();
   previousSlide();
 };
@@ -71,7 +71,7 @@ nextBtn.addEventListener('click', nextClickHandler);
 prevBtn.addEventListener('click', prevClickHandler);
 
 // indicators
-var indClickHandler = function (e) {
+let indClickHandler = function (e) {
   let target = e.target;
 
   if ( target.classList.contains('indicators__item') ) {
@@ -85,7 +85,7 @@ var indClickHandler = function (e) {
 indContainer.addEventListener('click', indClickHandler);
 
 // set keyboard controls
-var keyControlHandler = function (e) {
+let keyControlHandler = function (e) {
   if (e.keyCode === LEFT_ARROW) { prevClickHandler(); }
   if (e.keyCode === RIGHT_ARROW) { nextClickHandler(); }
   if (e.keyCode === SPACE) { pauseClickHandler(); }
