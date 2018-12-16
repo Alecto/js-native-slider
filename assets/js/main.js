3d090e67ab7fbad96ef13c2dbe5d20acfc99cb91
Alecto <fomenkoandrey@inbox.ru>
2018-12-16 21:59:26 +0200

    оптимизация кода.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,16 @@
 'use strict';
 
-let slideItems    = document.querySelectorAll('.slides__item'),
-    indContainer  = document.querySelector('.indicators'),
-    indItems      = document.querySelectorAll('.indicators__item'),
-    currentSlide  = 0;
+let slideItems  = document.querySelectorAll('.slides__item'),
+    indContainer = document.querySelector('.indicators'),
+    indItems    = document.querySelectorAll('.indicators__item'),
+    currentSlide = 0,
+    carouselInterval = 2000;
 
-const LEFT_ARROW  = 'ArrowLeft',
-      RIGHT_ARROW = 'ArrowRight',
-      SPACE       = ' ',
-      FA_PAUSE    = '<i class="fas fa-pause"></i>',
-      FA_PLAY     = '<i class="fas fa-play"></i>';
+const SPACE     = ' ',
+    LEFT_ARROW  = 'ArrowLeft',
+    RIGHT_ARROW = 'ArrowRight',
+    FA_PAUSE    = '<i class="fas fa-pause"></i>',
+    FA_PLAY     = '<i class="fas fa-play"></i>';
 
 // activate controls, if javascript is enabled
 indContainer.style.display = 'flex'; // flex
@@ -24,13 +25,9 @@ let gotoSlide = (n) => {
     indItems[currentSlide].classList.toggle('active');
 };
 
-let nextSlide = () => {
-    gotoSlide(currentSlide + 1);
-};
+let nextSlide = () => gotoSlide(currentSlide + 1);
 
-let previousSlide = () => {
-    gotoSlide(currentSlide - 1);
-};
+let previousSlide = () => gotoSlide(currentSlide - 1);
 
 let pauseSlideShow = () => {
     pausePlayBtn.innerHTML = FA_PAUSE;
@@ -41,10 +38,10 @@ let pauseSlideShow = () => {
 let playSlideShow = () => {
     pausePlayBtn.innerHTML = FA_PLAY;
     playbackStatus = true;
-    slideInterval = setInterval(nextSlide, 2000);
+    slideInterval = setInterval(nextSlide, carouselInterval);
 };
 
-let slideInterval = setInterval(nextSlide, 2000);
+let slideInterval = setInterval(nextSlide, carouselInterval);
 
 // controls
 let playbackStatus = true,
@@ -52,9 +49,7 @@ let playbackStatus = true,
     nextBtn  = document.querySelector('.controls__next'),
     prevBtn  = document.querySelector('.controls__prev');
 
-let pauseClickHandler = () => {
-    playbackStatus ? pauseSlideShow() : playSlideShow();
-};
+let pausePlayClickHandler = () => playbackStatus ? pauseSlideShow() : playSlideShow();
 
 let nextClickHandler = () => {
     pauseSlideShow();
@@ -66,7 +61,7 @@ let prevClickHandler = () => {
     previousSlide();
 };
 
-pausePlayBtn.addEventListener('click', pauseClickHandler);
+pausePlayBtn.addEventListener('click', pausePlayClickHandler);
 nextBtn.addEventListener('click', nextClickHandler);
 prevBtn.addEventListener('click', prevClickHandler);
 
@@ -87,7 +82,7 @@ indContainer.addEventListener('click', indClickHandler);
 let keyControlHandler = (e) => {
     if (e.key === LEFT_ARROW) { prevClickHandler(); }
     if (e.key === RIGHT_ARROW) { nextClickHandler(); }
-    if (e.key === SPACE) { pauseClickHandler(); }
+    if (e.key === SPACE) { pausePlayClickHandler(); }
 };
 
 document.addEventListener('keydown', keyControlHandler);
