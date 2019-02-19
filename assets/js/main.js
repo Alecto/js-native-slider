d7dcc2cf43aba05132a373cd04f7bc8871cfec34
Alecto <fomenkoandrey@inbox.ru>
2019-02-19 09:34:51 +0200

    playbackStatus = !playbackStatus;

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,14 +37,14 @@ let slideInterval = setInterval(gotoNextSlide, carouselInterval);
 let pauseSlideShow = () => {
     if (playbackStatus) {
         $pausePlayBtn.html(FA_PAUSE);
-        playbackStatus = false;
+        playbackStatus = !playbackStatus;
         clearInterval(slideInterval);
     }
 };
 
 let playSlideShow = () => {
     $pausePlayBtn.html(FA_PLAY);
-    playbackStatus = true;
+    playbackStatus = !playbackStatus;
     slideInterval = setInterval(gotoNextSlide, carouselInterval);
 };
 
