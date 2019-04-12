4edf722556a5c3a3296515fba69302eee74e08eb
Andrey Fomenko <fomenkoandrey1978@gmail.com>
2019-04-12 15:23:56 +0300

    Обновление FA, расческа стилей и JS.

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,11 +16,11 @@ document.querySelector('.controls').style.display = 'block'; // block
 
 // carousel basic engine
 let gotoNSlide = (n) => {
-    slideItems[currentSlide].classList.toggle('active');
-    indItems[currentSlide].classList.toggle('active');
-    currentSlide = (n + slideItems.length) % slideItems.length;
-    slideItems[currentSlide].classList.toggle('active');
-    indItems[currentSlide].classList.toggle('active');
+  slideItems[currentSlide].classList.toggle('active');
+  indItems[currentSlide].classList.toggle('active');
+  currentSlide = (n + slideItems.length) % slideItems.length;
+  slideItems[currentSlide].classList.toggle('active');
+  indItems[currentSlide].classList.toggle('active');
 };
 
 let gotoNextSlide = () => gotoNSlide(currentSlide + 1);
@@ -35,29 +35,29 @@ let prevBtn = document.querySelector('#prev-btn');
 let slideInterval = setInterval(gotoNextSlide, carouselInterval);
 
 let pauseSlideShow = () => {
-    if (playbackStatus) {
-        pausePlayBtn.innerHTML = FA_PLAY;
-        playbackStatus = !playbackStatus;
-        clearInterval(slideInterval);
-    }
+  if (playbackStatus) {
+    pausePlayBtn.innerHTML = FA_PLAY;
+    playbackStatus = !playbackStatus;
+    clearInterval(slideInterval);
+  }
 };
 
 let playSlideShow = () => {
-    pausePlayBtn.innerHTML = FA_PAUSE;
-    playbackStatus = !playbackStatus;
-    slideInterval = setInterval(gotoNextSlide, carouselInterval);
+  pausePlayBtn.innerHTML = FA_PAUSE;
+  playbackStatus = !playbackStatus;
+  slideInterval = setInterval(gotoNextSlide, carouselInterval);
 };
 
 let clickPausePlayBtn = () => playbackStatus ? pauseSlideShow() : playSlideShow();
 
 let clickNextBtn = () => {
-    pauseSlideShow();
-    gotoNextSlide();
+  pauseSlideShow();
+  gotoNextSlide();
 };
 
 let clickPrevBtn = () => {
-    pauseSlideShow();
-    gotoPrevSlide();
+  pauseSlideShow();
+  gotoPrevSlide();
 };
 
 pausePlayBtn.addEventListener('click', clickPausePlayBtn);
@@ -66,12 +66,12 @@ prevBtn.addEventListener('click', clickPrevBtn);
 
 // indicators
 let clickIndicatorBtn = (e) => {
-    let target = e.target;
+  let target = e.target;
 
-    if (target.classList.contains('indicator')) {
-        pauseSlideShow();
-        gotoNSlide(+target.getAttribute('data-slide-to'));
-    }
+  if (target.classList.contains('indicator')) {
+    pauseSlideShow();
+    gotoNSlide(+target.getAttribute('data-slide-to'));
+  }
 };
 
 // use delegation to optimize the event handler
@@ -79,9 +79,9 @@ indContainer.addEventListener('click', clickIndicatorBtn);
 
 // set keyboard controls
 let pressKeyControl = (e) => {
-    if (e.key === LEFT_ARROW) clickPrevBtn();
-    if (e.key === RIGHT_ARROW) clickNextBtn();
-    if (e.key === SPACE) clickPausePlayBtn();
+  if (e.key === LEFT_ARROW) clickPrevBtn();
+  if (e.key === RIGHT_ARROW) clickNextBtn();
+  if (e.key === SPACE) clickPausePlayBtn();
 };
 
 document.addEventListener('keydown', pressKeyControl);
