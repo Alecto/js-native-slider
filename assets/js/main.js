38c497895e73fff5e726ae5747bb00cc273ac4ef
Alecto <fomenkoandrey1978@gmail.com>
2020-03-23 08:40:23 +0200

    Оптимизация имен, кода. Слайдер переписан через паттерн "модуль".

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,105 +1,113 @@
-let slideItems = document.querySelectorAll('.slide');
-let indContainer = document.querySelector('.indicators');
-let indItems = document.querySelectorAll('.indicator');
-let currentSlide = 0;
-let carouselInterval = 2000;
+(function(time) {
+  let carousel = document.querySelector('.carousel');
+  let slides = carousel.querySelectorAll('.slide');
+  let indicatorsContainer = carousel.querySelector('.indicators');
+  let indicators = indicatorsContainer.querySelectorAll('.indicator');
+  let controls = carousel.querySelector('.controls');
+  let pausePlayBtn = controls.querySelector('#pause-play-btn');
+  let nextBtn = controls.querySelector('#next-btn');
+  let prevBtn = controls.querySelector('#prev-btn');
 
-const SPACE = ' ';
-const LEFT_ARROW = 'ArrowLeft';
-const RIGHT_ARROW = 'ArrowRight';
-const FA_PAUSE = '<i class="far fa-pause-circle"></i>';
-const FA_PLAY = '<i class="far fa-play-circle"></i>';
+  let currentSlide = 0;
+  let interval = time;
+  let isPlay = true;
+  let timerID = null;
+  let swipeStartX = null;
+  let swipeEndX = null;
 
-// activate controls, if javascript is enabled
-indContainer.style.display = 'flex'; // flex
-document.querySelector('.controls').style.display = 'block'; // block
+  const SPACE = ' ';
+  const LEFT_ARROW = 'ArrowLeft';
+  const RIGHT_ARROW = 'ArrowRight';
+  const FA_PAUSE = '<i class="far fa-pause-circle"></i>';
+  const FA_PLAY = '<i class="far fa-play-circle"></i>';
 
 // carousel basic engine
-let gotoNSlide = (n) => {
-  slideItems[currentSlide].classList.toggle('active');
-  indItems[currentSlide].classList.toggle('active');
-  currentSlide = (n + slideItems.length) % slideItems.length;
-  slideItems[currentSlide].classList.toggle('active');
-  indItems[currentSlide].classList.toggle('active');
-};
+  let gotoNSlide = (n) => {
+    slides[currentSlide].classList.toggle('active');
+    indicators[currentSlide].classList.toggle('active');
+    currentSlide = (n + slides.length) % slides.length;
+    slides[currentSlide].classList.toggle('active');
+    indicators[currentSlide].classList.toggle('active');
+  };
 
-let gotoNextSlide = () => gotoNSlide(currentSlide + 1);
+  let gotoNextSlide = () => gotoNSlide(currentSlide + 1);
 
-let gotoPrevSlide = () => gotoNSlide(currentSlide - 1);
+  let gotoPrevSlide = () => gotoNSlide(currentSlide - 1);
 
 // controls
-let playbackStatus = true;
-let pausePlayBtn = document.querySelector('#pause-play-btn');
-let nextBtn = document.querySelector('#next-btn');
-let prevBtn = document.querySelector('#prev-btn');
-let slideInterval = setInterval(gotoNextSlide, carouselInterval);
+  let pause = () => {
+    if (isPlay) {
+      pausePlayBtn.innerHTML = FA_PLAY;
+      isPlay = !isPlay;
+      clearInterval(timerID);
+    }
+  };
 
-let pauseSlideShow = () => {
-  if (playbackStatus) {
-    pausePlayBtn.innerHTML = FA_PLAY;
-    playbackStatus = !playbackStatus;
-    clearInterval(slideInterval);
-  }
-};
+  let play = () => {
+    pausePlayBtn.innerHTML = FA_PAUSE;
+    isPlay = !isPlay;
+    timerID = setInterval(gotoNextSlide, interval);
+  };
 
-let playSlideShow = () => {
-  pausePlayBtn.innerHTML = FA_PAUSE;
-  playbackStatus = !playbackStatus;
-  slideInterval = setInterval(gotoNextSlide, carouselInterval);
-};
+  let clickPause = () => isPlay ? pause() : play();
 
-let clickPausePlayBtn = () => playbackStatus ? pauseSlideShow() : playSlideShow();
+  let clickNext = () => {
+    pause();
+    gotoNextSlide();
+  };
 
-let clickNextBtn = () => {
-  pauseSlideShow();
-  gotoNextSlide();
-};
-
-let clickPrevBtn = () => {
-  pauseSlideShow();
-  gotoPrevSlide();
-};
-
-pausePlayBtn.addEventListener('click', clickPausePlayBtn);
-nextBtn.addEventListener('click', clickNextBtn);
-prevBtn.addEventListener('click', clickPrevBtn);
+  let clickPrev = () => {
+    pause();
+    gotoPrevSlide();
+  };
 
 // indicators
-let clickIndicatorBtn = (e) => {
-  let target = e.target;
+  let clickIndicatorBtn = (e) => {
+    let target = e.target;
 
-  if (target.classList.contains('indicator')) {
-    pauseSlideShow();
-    gotoNSlide(+target.getAttribute('data-slide-to'));
-  }
-};
-
-// use delegation to optimize the event handler
-indContainer.addEventListener('click', clickIndicatorBtn);
+    if (target.classList.contains('indicator')) {
+      pause();
+      gotoNSlide(+target.getAttribute('data-slide-to'));
+    }
+  };
 
 // set keyboard controls
-let pressKeyControl = (e) => {
-  if (e.key === LEFT_ARROW) clickPrevBtn();
-  if (e.key === RIGHT_ARROW) clickNextBtn();
-  if (e.key === SPACE) clickPausePlayBtn();
-};
-
-document.addEventListener('keydown', pressKeyControl);
+  let pressKey = (e) => {
+    if (e.key === LEFT_ARROW) clickPrev();
+    if (e.key === RIGHT_ARROW) clickNext();
+    if (e.key === SPACE) clickPause();
+  };
 
 // add swipe support
-let carousel = document.querySelector('.carousel');
-let swipeStartX = null;
-let swipeEndX = null;
+  let swipeStart = (e) => {
+    swipeStartX = e.changedTouches[0].pageX;
+  };
 
-let swipeStart = (e) => {
-  swipeStartX = e.changedTouches[0].pageX;
-};
+  let swipeEnd = (e) => {
+    swipeEndX = e.changedTouches[0].pageX;
+    swipeStartX - swipeEndX >  100 && clickPrev();
+    swipeStartX - swipeEndX < -100 && clickNext();
+  };
 
-let swipeEnd = (e) => {
-  swipeEndX = e.changedTouches[0].pageX;
-  swipeStartX - swipeEndX >  100 && clickPrevBtn();
-  swipeStartX - swipeEndX < -100 && clickNextBtn();
-};
+  // listeners activation
+  let setListeners = () => {
+    pausePlayBtn.addEventListener('click', clickPause);
+    nextBtn.addEventListener('click', clickNext);
+    prevBtn.addEventListener('click', clickPrev);
+    indicatorsContainer.addEventListener('click', clickIndicatorBtn);
+    carousel.addEventListener('touchstart', swipeStart);
+    carousel.addEventListener('touchend', swipeEnd);
+    document.addEventListener('keydown', pressKey);
+  };
 
-carousel.addEventListener('touchstart', swipeStart);
-carousel.addEventListener('touchend', swipeEnd);
+  // activate controls, if javascript is enabled
+  let init = () => {
+    indicatorsContainer.style.display = 'flex'; // flex
+    controls.style.display = 'block'; // block
+    setListeners();
+    timerID = setInterval(gotoNextSlide, interval);
+  };
+
+  init();
+
+})(2000);
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,25 @@
-'use strict';
-
 require('../../gulp/gulp-init.js')({HTML: '.'});
 
-const comb = require('../../gulp/tasks/comb.js'),
-    scss     = require('../../gulp/tasks/scss.js').scss,
-    mincss   = require('../../gulp/tasks/mincss.js'),
-    uglifyes = require('../../gulp/tasks/uglify.js').uglifyes,
-    { sync, syncInit } = require('../../gulp/tasks/sync.js');
+const comb = require('../../gulp/tasks/comb');
+const cs = require('../../gulp/tasks/create-structure');
+const {scss, scssDev} = require('../../gulp/tasks/scss.js');
+const mincss = require('../../gulp/tasks/mincss');
+const uglifyes = require('../../gulp/tasks/uglify').uglifyes;
+const {sync, syncInit} = require('../../gulp/tasks/sync');
 
-function watchFiles () {
-    syncInit();
-    watch($.path.scss.files, series(scss, mincss));
-    watch([$.path.js.files, '!' + $.path.js.filesMin], series(uglifyes, sync));
-    watch($.path.html.files, sync);
+function watchFiles() {
+  syncInit();
+  watch($.PATH.scss.files, series(scss));
+  watch([
+    $.PATH.js.files,
+    `!${$.PATH.js.filesMin}`
+  ], sync);
+  watch($.PATH.html.files, sync);
 }
 
+task('cs', cs);
 task('combScss', comb);
 task('uglifyEs6', series(uglifyes, sync));
 task('sass', series(scss));
+task('sassDev', series(scssDev));
 task('watch', watchFiles);
