0e6b2312d0c22637233da286ca4a56a83dc9b2ce
Alecto <fomenkoandrey1978@gmail.com>
2022-06-26 14:20:44 +0300

    оптимизация кодаЮ добавление favicon и прочие мелочи

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,7 @@
-
-
 require('../../../gulp/gulp-init.js')({HTML: '.'});
 
 const comb = require('../../../gulp/tasks/comb.js'),
   scssDC = require('../../../gulp/tasks/scss.js').scssDC,
-  mincss = require('../../../gulp/tasks/mincss.js'),
-  uglifyes = require('../../../gulp/tasks/uglify.js').uglifyes,
   {sync, syncInit} = require('../../../gulp/tasks/sync.js');
 
 function watchFiles() {
@@ -16,6 +12,5 @@ function watchFiles() {
 }
 
 task('combScss', comb);
-task('uglifyEs6', series(uglifyes, sync));
 task('sass', series(scssDC));
 task('watch', watchFiles);
