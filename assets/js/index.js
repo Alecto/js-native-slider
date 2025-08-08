382b582c42ed72b5afeeeea2651b87e3ed8cc445
Alecto <fomenkoandrey1978@gmail.com>
2025-08-08 20:12:37 +0300

    refactor: convert Carousel to class-based structure and enhance event handling

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,46 +1,57 @@
-function Carousel() {}
+class Carousel {
+  constructor(o) {
+    const settings = { ...{ containerId: '#carousel', slideId: '.slide', interval: 5000, isPlaying: true }, ...o }
 
-Carousel.prototype = {
+    this.container = document.querySelector(settings.containerId)
+    this.slides = this.container.querySelectorAll(settings.slideId)
+    this.TIMER_INTERVAL = settings.interval
+    this.isPlaying = settings.isPlaying
+  }
+
   _initProps() {
-    this.container = document.querySelector('#carousel')
     this.slidesContainer = this.container.querySelector('#slides-container')
-    this.slides = this.slidesContainer.querySelectorAll('.slide')
 
-    this.CODE_SPACE = 'Space'
-    this.CODE_ARROW_LEFT = 'ArrowLeft'
-    this.CODE_ARROW_RIGHT = 'ArrowRight'
     this.SLIDES_COUNT = this.slides.length
-    this.TIMER_INTERVAL = 2000
+    this.CODE_ARROW_RIGHT = 'ArrowRight'
+    this.CODE_ARROW_LEFT = 'ArrowLeft'
+    this.CODE_SPACE = 'Space'
+    this.FA_PAUSE = '<i class="fas fa-pause"></i>'
+    this.FA_PLAY = '<i class="fas fa-play"></i>'
+    this.FA_PREV = '<i class="fas fa-chevron-left"></i>'
+    this.FA_NEXT = '<i class="fas fa-chevron-right"></i>'
     this.SWIPE_THRESHOLD = 100
 
     this.currentSlide = 0
-    this.isPaused = true
+    this.isPlaying = true
     this.timerId = null
     this.swipeStartX = null
     this.swipeEndX = null
-  },
+  }
 
   _initControls() {
     const controlsContainer = document.createElement('div')
-    controlsContainer.setAttribute('id', 'controls-container')
     controlsContainer.classList.add('controls')
+    controlsContainer.setAttribute('id', 'controls-container')
 
-    const PAUSE_BTN = '<div id="pause-btn" class="control control-pause">Pause</div>'
-    const PREV_BTN = '<div id="prev-btn" class="control control-prev">Prev</div>'
-    const NEXT_BTN = '<div id="next-btn" class="control control-next">Next</div>'
+    const PAUSE_BTN = `<div class="control control-pause" id="pause-btn">${this.FA_PAUSE}</div>`
+    const PREV_BTN = `<div class="control control-prev" id="prev-btn">${this.FA_PREV}</div>`
+    const NEXT_BTN = `<div class="control control-next" id="next-btn">${this.FA_NEXT}</div>`
+
     controlsContainer.innerHTML = PAUSE_BTN + PREV_BTN + NEXT_BTN
 
     this.container.append(controlsContainer)
 
-    this.pauseBtn = this.container.querySelector('#pause-btn')
-    this.prevBtn = this.container.querySelector('#prev-btn')
-    this.nextBtn = this.container.querySelector('#next-btn')
-  },
+    this.pauseButton = this.container.querySelector('#pause-btn')
+    this.nextButton = this.container.querySelector('#next-btn')
+    this.prevButton = this.container.querySelector('#prev-btn')
+  }
 
   _initIndicators() {
     const indicatorsContainer = document.createElement('div')
-    indicatorsContainer.setAttribute('id', 'indicators-container')
     indicatorsContainer.classList.add('indicators')
+    indicatorsContainer.setAttribute('id', 'indicators-container')
+
+    this.container.append(indicatorsContainer)
 
     for (let i = 0; i < this.SLIDES_COUNT; i++) {
       const indicator = document.createElement('div')
@@ -49,19 +60,17 @@ Carousel.prototype = {
       indicatorsContainer.append(indicator)
     }
 
-    this.container.append(indicatorsContainer)
-
     this.indicatorsContainer = this.container.querySelector('#indicators-container')
-    this.indicators = this.indicatorsContainer.querySelectorAll('.indicator')
-  },
+    this.indicators = this.container.querySelectorAll('.indicator')
+  }
 
   _initEventListeners() {
-    this.pauseBtn.addEventListener('click', this.pausePlayHandler.bind(this))
-    this.prevBtn.addEventListener('click', this.prevHandler.bind(this))
-    this.nextBtn.addEventListener('click', this.nextHandler.bind(this))
+    this.pauseButton.addEventListener('click', this.pausePlayHandler.bind(this))
+    this.nextButton.addEventListener('click', this.nextHandler.bind(this))
+    this.prevButton.addEventListener('click', this.prevHandler.bind(this))
     this.indicatorsContainer.addEventListener('click', this._indicatorClickHandler.bind(this))
     document.addEventListener('keydown', this._keydownHandler.bind(this))
-  },
+  }
 
   _gotoNth(n) {
     this.slides[this.currentSlide].classList.toggle('active')
@@ -69,63 +78,66 @@ Carousel.prototype = {
     this.currentSlide = (n + this.SLIDES_COUNT) % this.SLIDES_COUNT
     this.slides[this.currentSlide].classList.toggle('active')
     this.indicators[this.currentSlide].classList.toggle('active')
-  },
-
-  _gotoPrev() {
-    this._gotoNth(this.currentSlide - 1)
-  },
+  }
 
   _gotoNext() {
     this._gotoNth(this.currentSlide + 1)
-  },
+  }
+
+  _gotoPrev() {
+    this._gotoNth(this.currentSlide - 1)
+  }
 
   _tick() {
     this.timerId = setInterval(() => this._gotoNext(), this.TIMER_INTERVAL)
-  },
+  }
 
   _indicatorClickHandler(e) {
-    const target = e.target
-    if (target.classList.contains('indicator')) {
+    const { target } = e
+
+    if (target && target.classList.contains('indicator')) {
+      const slideTo = +target.dataset.slideTo
       this.pauseHandler()
-      this._gotoNth(+target.dataset.slideTo)
+      this._gotoNth(slideTo)
     }
-  },
+  }
 
   _keydownHandler(e) {
-    if (e.code === this.CODE_SPACE) {
+    const { code } = e
+    if (code === this.CODE_ARROW_RIGHT) this.nextHandler()
+    if (code === this.CODE_ARROW_LEFT) this.prevHandler()
+    if (code === this.CODE_SPACE) {
       e.preventDefault()
       this.pausePlayHandler()
     }
-    if (e.code === this.CODE_ARROW_LEFT) this.prevHandler()
-    if (e.code === this.CODE_ARROW_RIGHT) this.nextHandler()
-  },
+  }
 
   playHandler() {
-    this.pauseBtn.textContent = 'Pause'
-    this.isPaused = true
+    this.pauseButton.innerHTML = this.FA_PAUSE
+    this.isPlaying = true
     this._tick()
-  },
+  }
 
   pauseHandler() {
-    if (!this.isPaused) return
-    this.pauseBtn.textContent = 'Play'
-    this.isPaused = false
+    if (!this.isPlaying) return
+    this.isPlaying = false
+    this.pauseButton.innerHTML = this.FA_PLAY
     clearInterval(this.timerId)
-  },
+  }
 
   pausePlayHandler() {
-    this.isPaused ? this.pauseHandler() : this.playHandler()
-  },
+    this.isPlaying ? this.pauseHandler() : this.playHandler()
+  }
 
   prevHandler() {
     this.pauseHandler()
     this._gotoPrev()
-  },
+  }
 
   nextHandler() {
     this.pauseHandler()
     this._gotoNext()
-  },
+  }
 
   init() {
     this._initProps()
@@ -136,38 +148,36 @@ Carousel.prototype = {
   }
 }
 
-Carousel.prototype.constructor = Carousel
+class SwipeCorousel extends Carousel {
+  constructor(options) {
+    super(options)
+  }
 
-function SwipeCarousel() {
-  Carousel.apply(this)
+  _initEventListeners() {
+    Carousel.prototype._initEventListeners.apply(this)
+    this.slidesContainer.addEventListener('touchstart', this._swipeStartHandler.bind(this))
+    this.slidesContainer.addEventListener('touchend', this._swipeEndHandler.bind(this))
+    this.slidesContainer.addEventListener('mousedown', this._swipeStartHandler.bind(this))
+    this.slidesContainer.addEventListener('mouseup', this._swipeEndHandler.bind(this))
+  }
+
+  _swipeStartHandler(e) {
+    this.swipeStartX = e instanceof MouseEvent ? e.clientX : e.changedTouches[0].clientX
+  }
+
+  _swipeEndHandler(e) {
+    this.swipeEndX = e instanceof MouseEvent ? e.clientX : e.changedTouches[0].clientX
+
+    const diffX = this.swipeEndX - this.swipeStartX
+
+    if (diffX > this.SWIPE_THRESHOLD) this.prevHandler()
+    if (diffX < -this.SWIPE_THRESHOLD) this.nextHandler()
+  }
 }
 
-SwipeCarousel.prototype = Object.create(Carousel.prototype)
-SwipeCarousel.prototype.constructor = SwipeCarousel
-
-SwipeCarousel.prototype._initEventListeners = function () {
-  Carousel.prototype._initEventListeners.apply(this)
-  this.slidesContainer.addEventListener('touchstart', this._swipeStartHandler.bind(this), { passive: true })
-  this.slidesContainer.addEventListener('mousedown', this._swipeStartHandler.bind(this))
-  this.slidesContainer.addEventListener('touchend', this._swipeEndHandler.bind(this))
-  this.slidesContainer.addEventListener('mouseup', this._swipeEndHandler.bind(this))
-}
-
-SwipeCarousel.prototype._swipeStartHandler = function (e) {
-  this.swipeStartX = e instanceof MouseEvent ? e.clientX : e.changedTouches[0].clientX
-}
-
-SwipeCarousel.prototype._swipeEndHandler = function (e) {
-  this.swipeEndX = e instanceof MouseEvent ? e.clientX : e.changedTouches[0].clientX
-
-  const diffX = this.swipeEndX - this.swipeStartX
-
-  if (diffX > this.SWIPE_THRESHOLD) this.prevHandler()
-  if (diffX < -this.SWIPE_THRESHOLD) this.nextHandler()
-}
-
-const carousel = new SwipeCarousel()
-
-console.log(carousel)
+const carousel = new SwipeCorousel({
+  slideId: '.item',
+  interval: 1000
+})
 
 carousel.init()
